7894de537f8af3b0839d83ff874071cb82c59e17
JosebaGmz <josebagomezcaballero90@gmail.com>
2024-11-11 23:34:06 +0100

    leap

--- leap/src/main/java/Leap.java
+++ leap/src/main/java/Leap.java
@@ -1,11 +1,7 @@
 class Leap {
 
     boolean isLeapYear(int year) {
-        boolean result = false;
-        if(year % 4 == 0 && year % 100 != 0 || year % 100 == 0 && year % 400 == 0){
-            result = true;
-        }
-        return result;
+        return year % 4 == 0 && year % 100 != 0 || year % 100 == 0 && year % 400 == 0;
     }
 
 }
